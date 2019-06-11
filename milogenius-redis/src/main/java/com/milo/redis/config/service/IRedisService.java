4d2d7221d0982f35a07262c29a838efd1f9960df
麦洛 <900324milo>
2019-06-11 18:01:44 +0800

    整合redis

---
+++ milogenius-redis/src/main/java/com/milo/redis/config/service/IRedisService.java
@@ -0,0 +1,9 @@
+package com.milo.redis.config.service;
+
+/**
+ * @author: Milogenius
+ * @create: 2019-06-11 18:00
+ * @description:
+ **/
+public interface IRedisService {
+}
---
+++ milogenius-redis/src/main/java/com/milo/redis/config/service/impl/RedisServiceImpl.java
@@ -0,0 +1,11 @@
+package com.milo.redis.config.service.impl;
+
+import com.milo.redis.config.service.IRedisService;
+
+/**
+ * @author: Milogenius
+ * @create: 2019-06-11 18:01
+ * @description:
+ **/
+public class RedisServiceImpl implements IRedisService {
+}
