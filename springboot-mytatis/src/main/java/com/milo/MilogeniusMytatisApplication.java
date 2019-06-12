e85caee15867e10f437ffe3978923ffaa666129e
麦洛 <900324milo>
2019-06-12 11:39:51 +0800

    整合mybatis

---
+++ springboot-mytatis/src/main/java/com/milo/MilogeniusMytatisApplication.java
@@ -0,0 +1,13 @@
+package com.milo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilogeniusMytatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilogeniusMytatisApplication.class, args);
+	}
+
+}
